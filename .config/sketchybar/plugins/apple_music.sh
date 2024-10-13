4c5db16cc446eca381327f8f97db803ecc19f11c
Jan Chroback <jan.chroback@icloud.com>
2024-10-13 20:46:50 +0200

    chore: removed debugging echo

--- .config/sketchybar/plugins/apple_music.sh
+++ .config/sketchybar/plugins/apple_music.sh
@@ -1,7 +1,5 @@
 OSA_CURRENTLY_PLAYING=$(osascript -e 'tell application "Music" to if it is running then get the name of the current track & " - " & the artist of the current track')
 
-echo "$current_track"
-
 if [ -n "$OSA_CURRENTLY_PLAYING" ]; then
     CURRENTLY_PLAYING=$OSA_CURRENTLY_PLAYING
 else
