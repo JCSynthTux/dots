855d9539768876fdc75d4f64d5de148be5e6f5df
Jan Chroback <jan.chroback@icloud.com>
2026-06-23 20:12:37 +0200

    ops: disable blur

--- .config/hypr/config/decorations.lua
+++ .config/hypr/config/decorations.lua
@@ -38,6 +38,7 @@ hl.config({
         inactive_opacity = 1,
         fullscreen_opacity = 1,
         blur = {
+            enabled = false,
             size = 5,
             passes = 4,
             special = true,
