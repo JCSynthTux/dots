9380c8df685b807f5028574e1e28cc173d2da0ff
Jan Chroback <jan.chroback@icloud.com>
2026-08-26 09:40:39 +0200

    ops: adjust ssh socket for keepass

--- .config/git-ssh-wrapper.sh
+++ .config/git-ssh-wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export SSH_AUTH_SOCK="/home/jchroback/.bitwarden-ssh-agent.sock"
+export SSH_AUTH_SOCK="/run/user/1000/ssh-agent.socket"
 exec /usr/bin/git "$@"
--- .config/hypr/config/environment.lua
+++ .config/hypr/config/environment.lua
@@ -1,2 +1,3 @@
 -- Environmental variables
 -- if you don't use UWSM, define your variables here (e.g. hl.env("QT_QPA_PLATFORM", "wayland"))
+hl.env("SSH_AUTH_SOCK", "/run/user/1000/ssh-agent.socket")
