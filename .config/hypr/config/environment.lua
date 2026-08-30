312dd0a55607588c11cbd244c32916f083416ca4
Jan Chroback <jan.chroback@icloud.com>
2026-08-30 17:37:47 +0200

    feat: add xdg scripts

--- .config/hypr/config/environment.lua
+++ .config/hypr/config/environment.lua
@@ -1,3 +1,4 @@
 -- Environmental variables
 -- if you don't use UWSM, define your variables here (e.g. hl.env("QT_QPA_PLATFORM", "wayland"))
 hl.env("SSH_AUTH_SOCK", "/run/user/1000/ssh-agent.socket")
+hl.env("QT_QPA_PLATFORMTHEME", "hyprqt6engine")
